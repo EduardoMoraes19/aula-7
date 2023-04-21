e39cae5d090d6e71cc8f0b60402088f41c890650
EduardoMoraes19 <eduardodev19@gmail.com>
2023-04-20 23:13:30 -0300

    feat: inciando projeto

---
+++ eleicoes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <?php
+    $ano = $_GET ["an"];
+    $idade = 2023 - $ano;
+    echo "Quem narceu em $ano tem idade de $idade anos.";
+    $tipo = ($idade>=18 && $idade<65)?"OBRIGATORIO":"NAO OBRIGATORIO";
+    echo "E dessa forma seu voto e $tipo"; 
+    ?>
+</div>    
+</body>
+</html>
